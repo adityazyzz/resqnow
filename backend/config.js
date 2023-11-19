f5da2015f1a213bc36083b1f49f72129909ff86e
Aditya <136951825+adityazyzz@users.noreply.github.com>
2023-11-20 02:44:56 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const database = "cloud-notes";
-const uri = `mongodb://localhost:27017/${database}`
-mongoose.connect(uri,()=>{
-    console.log("Connected to cloud-notes successfully !")
-});
